27f53b2dedcefa8105a6b112798ff47961417259
nvalenzu <nicolas.valenzuelam@alumnos.usm.cl>
2016-09-23 12:05:23 -0300

    se comienza action bar, se agregan algunas opciones

---
+++ Proyecto/app/src/main/java/com/multimedios/proyecto/proyecto/ActivityABFragment.java
@@ -0,0 +1,22 @@
+package com.multimedios.proyecto.proyecto;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ActivityABFragment extends Fragment {
+
+    public ActivityABFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_activity_ab, container, false);
+    }
+}
